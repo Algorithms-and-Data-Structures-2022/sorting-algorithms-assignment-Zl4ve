75d72b8a2efb0212ff1d80a9d9b1a8240e5e0d4b
Renat Mudarisov <mudarisov.renat58@gmail.com>
2022-05-13 15:52:07 +0300

    Done.

--- src/insertion_sort.cpp
+++ src/insertion_sort.cpp
@@ -9,37 +9,42 @@ namespace assignment {
   int linear_search(const std::vector<int>& arr, int index) {
 
     // итерация всех предыдущих элементов [0, index - 1] (они находятся в отсортированном порядке)
-    for (int curr_pos = -1 /* ... */; false /* условие ... */; /* обновление curr_pos ... */) {
-
+    for (int curr_pos = index - 1; curr_pos >= 0; curr_pos--) {
+      if (arr[index] >= arr[curr_pos]) {
+        return curr_pos + 1;
+      }
       // если текущий элемент меньше или равен вставляемому, позиция для вставки найдена ...
     }
-
-    return -1;  // здесь что-то не так ...
+    return 0;
   }
 
   int binary_search(const std::vector<int>& arr, int index) {
 
     // начало, конец и середина области поиска места для вставки [0, index - 1]
-    int start = -1 /* здесь что-то не так ... */;
-    int stop = -1 /* здесь что-то не так ... */;
-    int middle = -1 /* здесь что-то не так ... */;
+    int start = 0;
+    int stop = index - 1;
+    int middle = middle_of(start, stop);
 
     // ищем до тех пор, пока границы не схлопнулись
     while (start <= stop) {
 
       // возвращаем позицию для вставки
       if (arr[index] == arr[middle]) {
-        return -1 /* здесь что-то не так ... */;
+        return middle;
       }
 
       // обновляем границы области поиска ...
-
+      if (arr[index] > arr[middle]) {
+        start = middle + 1;
+      } else {
+        stop = middle - 1;
+      }
       // обновляем середину области поиска
-      middle = middle_of(-1, -1);  // здесь что-то не так ...
+      middle = middle_of(start, stop);
     }
 
     // в конечном счете возвращаем начало последней области поиска
-    return -1;  // здесь что-то не так ...
+    return start;  // здесь что-то не так ...
   }
 
   void InsertionSort::Sort(std::vector<int>& arr) const {
@@ -54,6 +59,15 @@ namespace assignment {
 
       // если индекс вставки не совпадает с текущей позицией элемента,
       // производим вставку элемента на вычисленную позицию (std::copy или цикл for) ...
+      if (index != ins_index) {
+        int ins = arr[index];
+
+        for (int i = index; i > ins_index; i--){
+          arr[i] = arr[i - 1];
+        }
+
+        arr[ins_index] = ins;
+      }
     }
   }
 
